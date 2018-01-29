7b8fe6ae55457b0dfabd9dd07c2704edf0f943f1
Michael Crandell <thomas.m.crandell@gmail.com>
2018-01-29 11:00:02 -0500

    changed .fill() syntax

--- rgb.py
+++ rgb.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
 import serial
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+DISPLAYSURF=pygame.display.set_mode((500,400), 0, 32)
+pygame.display.set_caption('RGB Lab')
 
 s=serial.Serial("/dev/ttyACM0")
 
@@ -8,10 +13,11 @@ while(True):
     l=s.readline()
     colors= l.rstrip().split(",")
     rgb= [int(val) for val in colors]
-    print rgb
+    
+    DISPLAYSURF.fill(rgb)
 
-
-    """
-    for i in range(len(colors)):
-        colors[i]=int(colors[i])
-    """
+    for event in pygame.event.get():
+      if event.type == QUIT:
+        pygame.quit()
+        sys.exit()
+    pygame.display.update()
